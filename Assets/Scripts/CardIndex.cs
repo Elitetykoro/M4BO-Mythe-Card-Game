c8787892b1346a31d14663340bae2054e9894255
julien oving <37832@ma-web.nl>
2024-05-15 09:33:27 +0200

    shit

--- Assets/Scripts/CardIndex.cs
+++ Assets/Scripts/CardIndex.cs
@@ -1,11 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using TMPro;
 public class CardIndex : MonoBehaviour
 {
     public Vector3 cardStartPos;
     public int cardIndex;
     public bool CanBeMoved = true;
     public int ATKleft, ATKright, ATKtop, ATKbottom;
+    [SerializeField] private TMP_Text AtkTextTop, AtkTextBottom, AtkTextRight, AtkTextLeft;
+    private void Start()
+    {
+        AtkTextTop.text = ATKtop.ToString();
+        AtkTextBottom.text = ATKbottom.ToString();
+        AtkTextLeft.text = ATKleft.ToString();
+        AtkTextRight.text = ATKright.ToString();
+    }
 }
